e574b77806d7f2740dbf69ef0e817f2ee1b84ed0
sai <knvsk18@gmail.com>
2022-12-13 12:14:22 +0530

    commit

---
+++ pracitce/src/com/orangehrm/operators.java
@@ -0,0 +1,16 @@
+package com.orangehrm;
+
+public class operators {
+	
+	public static void main(String[] args) {
+		
+		int a=10, b=20, c;
+		
+		c=a+=10;
+		System.out.println(c);
+		
+		
+		
+	}
+
+}
