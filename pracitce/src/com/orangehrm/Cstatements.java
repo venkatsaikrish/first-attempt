02f584d5cb1ed14adc959c21630924c984abe2ce
sai <knvsk18@gmail.com>
2022-12-15 18:14:36 +0530

    commit

---
+++ pracitce/src/com/orangehrm/Cstatements.java
@@ -0,0 +1,16 @@
+package com.orangehrm;
+
+
+public class Cstatements {
+	
+	public static void main(String[] args) {
+		
+		int a=20, b=10;
+		 if (a >b);
+			 
+		 
+		 System.out.println("a is big value ");
+		 
+	}
+
+}
