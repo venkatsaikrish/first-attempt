bac982bf339be0f3a4ab76275a3380ebdd329f31
sai <knvsk18@gmail.com>
2022-12-15 17:59:57 +0530

    commit

---
+++ pracitce/src/com/arrays/PracticeValue.java
@@ -0,0 +1,35 @@
+package com.arrays;
+
+public class PracticeValue {
+	
+	public static void main(String[] args) {
+		
+
+		int ivariable=10;
+		long lvariable=353434;
+		
+		float fvariable=12.43f;
+		double dvariable=324.35;
+		
+		boolean vflag=true;
+		
+		char ch=10112;
+
+		String name = new String();
+		String name1 = new String();
+		
+		
+		System.out.println(name.valueOf(vflag));
+		System.out.println(name1.valueOf(ivariable));
+		
+		System.out.println(name.valueOf(vflag) instanceof String );
+		System.out.println(name.valueOf(ivariable) instanceof String);
+		
+		
+		
+		
+		
+		
+	}
+
+}
