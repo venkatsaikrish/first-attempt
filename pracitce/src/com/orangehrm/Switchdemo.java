28752f43ae16dbf83a635af0a76c562591e9224c
sai <knvsk18@gmail.com>
2022-12-15 18:12:30 +0530

    review

---
+++ pracitce/src/com/orangehrm/Switchdemo.java
@@ -0,0 +1,37 @@
+package com.orangehrm;
+
+public class Switchdemo {
+	
+	public static void main(String[] args) {
+		
+		int marks=10;
+		
+		switch(marks) {
+		
+		case 50:
+		System.out.println("grade A");
+		break;
+		
+		case 40:
+		System.out.println("grade B");
+		break;
+		
+    	 case 30:
+   		System.out.println("grade C");
+   		break;
+   		
+				
+		case 20:
+		System.out.println("grade d");
+		break;
+		 
+		default:
+			
+			System.out.println("fail");
+		
+
+		    
+		}
+	}
+
+}
